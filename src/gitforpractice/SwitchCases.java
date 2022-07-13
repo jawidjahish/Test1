5776a19fdfdb3f501c89298659f68cf5b49da1ad
Jawid Jahish <jawidjahish@Jawids-MBP.attlocal.net>
2022-07-13 14:12:56 -0700

    switch cases

---
+++ src/gitforpractice/SwitchCases.java
@@ -0,0 +1,25 @@
+package gitforpractice;
+
+public class SwitchCases {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String str = "test";
+		
+		switch (str) {
+		case "test":
+			System.out.println("yes");
+		break;
+		case "test1":
+			System.out.println("NO");
+		break;
+		default:
+			System.out.println("default");
+		}
+
+	}
+
+}
